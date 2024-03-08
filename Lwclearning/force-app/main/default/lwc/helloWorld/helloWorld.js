b1b8b5ccf89901542d1cf8963ac9c5f804b9f867
Koyeli <krakshit@deloitte.com>
2024-03-08 20:40:46 +0530

    Third LWC Commit

--- Lwclearning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ Lwclearning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullname = "Koyeli Rakshit"
+    course = "Zero to Hero"
+    title = "Aura"
+    changeHandler(event){
+        this.title = event.target.value;
+    }
 }
